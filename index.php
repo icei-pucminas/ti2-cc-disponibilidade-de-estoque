84eb22debb36ce5e55ab46270fc924755c2896ee
markgomer <markgomer@users.noreply.github.com>
2020-11-27 19:41:59 -0300

    revert commit

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 <?php include_once('codigo/src/main/resources/index.html'); ?>
+=======
+<?php include_once('index.html'); ?>
+>>>>>>> parent of 1898065... Reorganização de arquivos para a pasta "codigo" e ligação entre as páginas html
